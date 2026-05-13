abf36d32340748b6d89d94e60b5dfa3a5fd2ebdb
Anja Franke <anja.franke@student.uni-tuebingen.de>
2026-05-13 14:23:15 +0200

    Co-authored-by: Samuel J. S. <Samoojo@users.noreply.github.com>

--- fitness_trainer.py
+++ fitness_trainer.py
@@ -2,3 +2,14 @@
 
 import activity_recognizer as activity
 import pyglet
+import pandas as pd
+
+
+df = pd.read_csv("diabetes.csv")
+
+brew list python
+
+
+
+/usr/local/Cellar/python@3.14/3.14.4_1/bin/python3.14 -m venv venv
+
